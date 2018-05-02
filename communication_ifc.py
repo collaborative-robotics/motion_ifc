47f9d1e71609ced63e387b807e2d9e243d22d2a0
Adnan Munawar <adnan.munawar@live.com>
2018-05-01 20:33:17 -0400

    Additional methods to get op_space and controller char from crtk_name

--- communication_ifc.py
+++ communication_ifc.py
@@ -6,7 +6,7 @@ from crkt_common import *
 class CommunicationIfc(object):
     def __init__(self, topic_name, data_type, enable_wd=False, queue_size=10):
         self.topic_name = topic_name
-        self.cmd = data_type()
+        self._data = data_type()
         self.enable_wd = enable_wd
         self.crtk_name = ''
         self.wd_time_out = 0.0
@@ -16,7 +16,7 @@ class CommunicationIfc(object):
         self.sub = rospy.Subscriber(topic_name, data_type, self.message_cb, queue_size=queue_size)
 
     def message_cb(self, data):
-        self.cmd = data
+        self._data = data
         self.last_received_time = rospy.Time.now().to_sec()
         if self.enable_wd:
             self.watch_dog.acknowledge_wd()
@@ -30,3 +30,15 @@ class CommunicationIfc(object):
     def get_crtk_name(self):
         self.crtk_name = interpret_crtk_name_from_topic(self.topic_name)
         return self.crtk_name
+
+    def get_mode_str(self):
+        crtk_name = self.get_crtk_name()
+        return crtk_name.split('_')[0]
+
+    def get_opspace_char(self):
+        crtk_name = self.get_crtk_name()
+        return crtk_name.split('_')[1][0]
+
+    def get_controller_char(self):
+        crtk_name = self.get_crtk_name()
+        return crtk_name.split('_')[1][1]
