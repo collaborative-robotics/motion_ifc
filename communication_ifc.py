8958eafe6e026a0b1a90cb4d873b415f53b314da
Adnan Munawar <adnan.munawar@live.com>
2018-05-03 19:55:19 -0400

    Scope changes and some code clarity

--- communication_ifc.py
+++ communication_ifc.py
@@ -5,28 +5,26 @@ from crkt_common import *
 
 class CommunicationIfc(object):
     def __init__(self, topic_name, data_type, enable_wd=False, queue_size=10, is_publisher=False):
-        self.topic_name = topic_name
+        self._topic_name = topic_name
         self._data = data_type()
-        self.enable_wd = enable_wd
-        self.crtk_name = ''
-        self.wd_time_out = 0.0
-        self.last_received_time = 0.0
-
+        self._enable_wd = enable_wd
+        self._crtk_name = ''
+        self._last_received_time = 0.0
         self._is_publisher = is_publisher
-        self.watch_dog = WatchDog(self.wd_time_out)
+        self._watch_dog = WatchDog()
         if is_publisher:
-            self.pub = rospy.Publisher(topic_name, data_type, queue_size=queue_size)
+            self._pub = rospy.Publisher(topic_name, data_type, queue_size=queue_size)
         else:
-            self.sub = rospy.Subscriber(topic_name, data_type, self.message_cb, queue_size=queue_size)
+            self._sub = rospy.Subscriber(topic_name, data_type, self.message_cb, queue_size=queue_size)
 
     def message_cb(self, data):
         self._data = data
-        self.last_received_time = rospy.Time.now().to_sec()
-        if self.enable_wd:
-            self.watch_dog.acknowledge_wd()
+        self._last_received_time = rospy.Time.now().to_sec()
+        if self._enable_wd:
+            self._watch_dog.acknowledge_wd()
 
     def is_active(self):
-        return not self.watch_dog.is_wd_expired()
+        return not self._watch_dog.is_expired()
 
     def get_data(self):
         if self._is_publisher:
@@ -41,13 +39,14 @@ class CommunicationIfc(object):
                 print 'WARN: Mismatched data-types, ignoring'
             else:
                 self._data = data
+                self._pub.publish(data)
 
     def get_last_received_time(self):
-        return self.last_received_time
+        return self._last_received_time
 
     def get_crtk_name(self):
-        self.crtk_name = interpret_crtk_name_from_topic(self.topic_name)
-        return self.crtk_name
+        self._crtk_name = interpret_crtk_name_from_topic(self._topic_name)
+        return self._crtk_name
 
     def get_mode_str(self):
         crtk_name = self.get_crtk_name()
@@ -60,3 +59,6 @@ class CommunicationIfc(object):
     def get_controller_char(self):
         crtk_name = self.get_crtk_name()
         return crtk_name.split('_')[1][1]
+
+    def disconnect(self):
+        self._sub.unregister()
