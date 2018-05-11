4dc010ffafd9e84d093b4f12bad327240d961d8d
Adnan Munawar <adnan.munawar@live.com>
2018-05-11 13:07:51 -0400

    Cleaning up

--- interp_logic.py
+++ interp_logic.py
@@ -9,9 +9,9 @@ class Interpolation(object):
         self._t0 = 0.0
         self._tf = 0.0
 
-        self._coefficients = np.zeros([1, 1])
-        self._T_mat = np.zeros([1, 1, 1])
-        self._boundary_conditions = np.zeros([1, 1])
+        self._coefficients = np.zeros([6, 1])
+        self._T_mat = np.zeros([6, 6, 1])
+        self._boundary_conditions = np.zeros([1, 6])
 
         self._dimensions = 1
         self._t_array = []
@@ -24,22 +24,22 @@ class Interpolation(object):
         pass
 
     def _compute_time_mat(self, t0, tf):
-        T_mat = np.mat([[1, t0, t0**2,     t0**3,      t0**4,      t0**5],
+        t_mat = np.mat([[1, t0, t0**2,     t0**3,      t0**4,      t0**5],
                         [0,  1,  2*t0, 3*(t0**2),  4*(t0**3),  5*(t0**4)],
                         [0,  0,     2,      6*t0, 12*(t0**2), 20*(t0**3)],
                         [1, tf, tf**2,     tf**3,      tf**4,      tf**5],
                         [0,  1,  2*tf, 3*(tf**2),  4*(tf**3),  5*(tf**4)],
                         [0,  0,     2,      6*tf, 12*(tf**2), 20*(tf**3)]])
-        return T_mat
+        return t_mat
 
     def get_interpolated_x_dx_ddx(self, t):
         return self.get_interpolated_x(t), self.get_interpolated_dx(t), self.get_interpolated_ddx(t)
 
     def get_interpolated_x(self, t):
         self._lock.acquire()
-        if not type(t) is np.array:
+        if not isinstance(t, np.ndarray):
             t = np.array(t)
-        # t = np.array(t)
+
         if t.size == 1:
             if not self._t0 - self._t0_moe <= t <= self._tf:
                 raise Exception('Time {} should be between {} and {}'.format(t, self._t0, self._tf))
@@ -60,9 +60,9 @@ class Interpolation(object):
 
     def get_interpolated_dx(self, t):
         self._lock.acquire()
-        if not type(t) is np.array:
+        if not isinstance(t, np.ndarray):
             t = np.array(t)
-        # t = np.array(t)
+
         if t.size == 1:
             if not self._t0 - self._t0_moe <= t <= self._tf:
                 raise Exception('Time {} should be between {} and {}'.format(t, self._t0, self._tf))
@@ -83,10 +83,9 @@ class Interpolation(object):
 
     def get_interpolated_ddx(self, t):
         self._lock.acquire()
-
-        if not type(t) is np.array:
+        if not isinstance(t, np.ndarray):
             t = np.array(t)
-        # t = np.array(t)
+
         if t.size == 1:
             if not self._t0 - self._t0_moe <= t <= self._tf:
                 raise Exception('Time {} should be between {} and {}'.format(t, self._t0, self._tf))
@@ -143,7 +142,7 @@ class Interpolation(object):
         self._t0 = t0
         self._tf = tf
 
-        t0_adjusted = 0.0
+        t0_adjusted = t0 - t0
         tf_adjusted = tf - t0
         if tf_adjusted <= 0:
             raise Exception('tf: {} cannot be less than t0: {}'.format(tf, t0))
