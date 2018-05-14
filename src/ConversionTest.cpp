c86f7dbd156ea5dbe97cbfba43dec18e2c6f04cf
Adnan Munawar <adnan.munawar@live.com>
2018-05-14 16:53:15 -0400

    Back and forth testing between serialization and de-seriization

--- src/ConversionTest.cpp
+++ src/ConversionTest.cpp
@@ -11,22 +11,38 @@ using namespace std;
 int main() {
     std::cout << "Testing Interpolation Logic using Eigen \n" << std::endl;
     RowVectorXd x0, dx0, ddx0, xf, dxf, ddxf;
-    DataConversion test_obj;
-    geometry_msgs::TransformStamped data;
-    data.transform.translation.x = 0.0;
-    data.transform.translation.y = 1.0;
-    data.transform.translation.z = 2.0;
-    data.transform.rotation.x = 0.0;
-    data.transform.rotation.y = 0.0;
-    data.transform.rotation.z = 0.0;
-    data.transform.rotation.w = 1.0;
-    sensor_msgs::JointState jdata;
-    test_obj.serialize(data);
-    std::cout << "Transform Data: \n" << test_obj.x << std::endl;
-    jdata.position.resize(3);
-    jdata.position[0] = 5.2;
-    jdata.position[1] = 4.2;
-    jdata.position[2] = -9.2;
-    test_obj.serialize(jdata);
-    std::cout << "Sensor Data: \n" << test_obj.x << std::endl;
+    DataConversion dataConversion;
+    geometry_msgs::TransformStamped t_stamped;
+    t_stamped.transform.translation.x = 0.0;
+    t_stamped.transform.translation.y = 1.0;
+    t_stamped.transform.translation.z = 2.0;
+    t_stamped.transform.rotation.x = 0.0;
+    t_stamped.transform.rotation.y = 0.0;
+    t_stamped.transform.rotation.z = 0.0;
+    t_stamped.transform.rotation.w = 1.0;
+    dataConversion.serialize(t_stamped);
+
+    std::cout << "Transform Data: \n" << dataConversion.x << std::endl;
+
+    geometry_msgs::TransformStamped read_t_stamped;
+    dataConversion.deserialize(&read_t_stamped);
+
+    std::cout << "Retrieved Transform Translation \n" << read_t_stamped.transform.translation << std::endl;
+    std::cout << "Retrieved Transform Rotation \n" << read_t_stamped.transform.rotation << std::endl;
+
+    sensor_msgs::JointState joint_state;
+    joint_state.position.resize(3);
+    joint_state.position[0] = 5.2;
+    joint_state.position[1] = 4.2;
+    joint_state.position[2] = -9.2;
+    dataConversion.serialize(joint_state);
+
+    std::cout << "Sensor Data: \n" << dataConversion.x << std::endl;
+
+    sensor_msgs::JointState read_joint_state;
+    dataConversion.deserialize(&read_joint_state);
+    std::cout << "Retrieved Joint State Data: \n";
+    for (int i = 0 ; i < read_joint_state.position.size() ; i++){
+    std::cout << "Joint[" << i << "]: " << read_joint_state.position[i] << std::endl;
+    }
 }
