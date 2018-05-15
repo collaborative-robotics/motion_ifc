73d88f088cd93d6223a568e63c098e8bb25355b2
Adnan Munawar <adnan.munawar@live.com>
2018-05-15 19:16:58 -0400

    Working example with retrieving methods by name

--- include/motion_ifc/Controllers.h
+++ include/motion_ifc/Controllers.h
@@ -13,8 +13,9 @@ class Interpolate: public DataConversion{
 public:
     Interpolate();
     ~Interpolate(){
-//        delete temp;
-        delete method_map["interpolate_cp"];
+        for (_method_iterator = method_map.begin(); _method_iterator != method_map.end() ; _method_iterator++){
+            delete _method_iterator->second;
+        }
     }
     void interpolate_cp(_cp_data_type &data);
     void interpolate_cr(_cr_data_type &data);
@@ -26,21 +27,60 @@ public:
     void interpolate_jv(_jv_data_type &data);
     void interpolate_jf(_jf_data_type &data);
 
-    std::map<std::string, FcnHandleBase*> method_map;
-//    FcnHandle<_cp_data_type>* temp;
+    FcnHandleBase* get_method_by_name(std::string method_name){
+        return method_map[method_name];
+    }
 
+private:
+    std::map<std::string, FcnHandleBase*> method_map;
+    std::map<std::string, FcnHandleBase*>::iterator _method_iterator;
 };
 
 Interpolate::Interpolate(){
-//    temp = new FcnHandle<_cp_data_type>(&Interpolate::interpolate_cp, this);
-//    temp->assign_fcn(&Interpolate::interpolate_cp, this);
-//    method_map["interpolate_cp"] = temp;
     method_map["interpolate_cp"] = new FcnHandle<_cp_data_type>(&Interpolate::interpolate_cp, this);
+    method_map["interpolate_cr"] = new FcnHandle<_cr_data_type>(&Interpolate::interpolate_cr, this);
+    method_map["interpolate_cv"] = new FcnHandle<_cv_data_type>(&Interpolate::interpolate_cv, this);
+    method_map["interpolate_cf"] = new FcnHandle<_cf_data_type>(&Interpolate::interpolate_cf, this);
+
+    method_map["interpolate_jp"] = new FcnHandle<_jp_data_type>(&Interpolate::interpolate_jp, this);
+    method_map["interpolate_jr"] = new FcnHandle<_jr_data_type>(&Interpolate::interpolate_jr, this);
+    method_map["interpolate_jv"] = new FcnHandle<_jv_data_type>(&Interpolate::interpolate_jv, this);
+    method_map["interpolate_jf"] = new FcnHandle<_jf_data_type>(&Interpolate::interpolate_jf, this);
 }
 
 void Interpolate::interpolate_cp(_cp_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
     std::cout << "Passed Transform is: \n" << data.transform.translation << std::endl;
 }
+
+void Interpolate::interpolate_cr(_cr_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_cv(_cv_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_cf(_cf_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_jp(_jp_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_jr(_jr_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_jv(_jv_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
+void Interpolate::interpolate_jf(_jf_data_type &data){
+    std::cout << "Called: " << __FUNCTION__ << std::endl;
+}
+
 
 class Move{
 public:
--- src/FcnHandleTest.cpp
+++ src/FcnHandleTest.cpp
@@ -3,11 +3,27 @@
 
 int main(){
     Interpolate intObj;
-    FcnHandle<_cp_data_type> * fcn;
-    fcn = (FcnHandle<_cp_data_type> *)intObj.method_map["interpolate_cp"];
-    _cp_data_type data;
-    data.transform.translation.x = 5.0;
-    (*fcn)(data);
+    FcnHandle<_cp_data_type> * fcn_cp = (FcnHandle<_cp_data_type> *)intObj.get_method_by_name("interpolate_cp");
+    FcnHandle<_cr_data_type> * fcn_cr = (FcnHandle<_cp_data_type> *)intObj.get_method_by_name("interpolate_cr");
+    FcnHandle<_cv_data_type> * fcn_cv = (FcnHandle<_cp_data_type> *)intObj.get_method_by_name("interpolate_cv");
+    FcnHandle<_cf_data_type> * fcn_cf = (FcnHandle<_cp_data_type> *)intObj.get_method_by_name("interpolate_cf");
+    FcnHandle<_jp_data_type> * fcn_jp = (FcnHandle<_jp_data_type> *)intObj.get_method_by_name("interpolate_jp");
+    FcnHandle<_jr_data_type> * fcn_jr = (FcnHandle<_jr_data_type> *)intObj.get_method_by_name("interpolate_jr");
+    FcnHandle<_jv_data_type> * fcn_jv = (FcnHandle<_jv_data_type> *)intObj.get_method_by_name("interpolate_jv");
+    FcnHandle<_jf_data_type> * fcn_jf = (FcnHandle<_jf_data_type> *)intObj.get_method_by_name("interpolate_jf");
+
+    _cp_data_type cp_data;
+    _jp_data_type jp_data;
+    cp_data.transform.translation.x = 5.0;
+    (*fcn_cp)(cp_data);
+    (*fcn_cr)(cp_data);
+    (*fcn_cv)(cp_data);
+    (*fcn_cf)(cp_data);
+    (*fcn_jp)(jp_data);
+    (*fcn_jr)(jp_data);
+    (*fcn_jv)(jp_data);
+    (*fcn_jf)(jp_data);
+
 
     return 0;
 }
